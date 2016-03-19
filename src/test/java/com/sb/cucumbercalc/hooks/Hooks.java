f30c6e4dfd7145161005ba40b1909aa84c40e7fb
sboursault <sebastien.boursault@gmail.com>
2016-03-19 22:36:41 +0100

    configuration to run tests against different environments

--- src/test/java/com/sb/cucumbercalc/hooks/Hooks.java
+++ src/test/java/com/sb/cucumbercalc/hooks/Hooks.java
@@ -3,24 +3,27 @@ package com.sb.cucumbercalc.hooks;
 
 import com.sb.cucumbercalc.support.ui.utils.EnhancedEventFiringWebDriver;
 import cucumber.api.java.Before;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.Resource;
 
 /**
  * Created by sboursault on 23/02/16.
  */
+
 public class Hooks {
 
     // urls
-    public static final String GOOGLE_URL = "https://www.google.fr";
+    @Value("${google.url}")
+    private String googleUrl;
 
     @Resource
     private EnhancedEventFiringWebDriver webDriver;
 
     @Before
     public void navigateToGooglePage() {
-        if (!webDriver.getCurrentUrl().startsWith(GOOGLE_URL)) {
-            webDriver.get(GOOGLE_URL);
+        if (!webDriver.getCurrentUrl().startsWith(googleUrl)) {
+            webDriver.get(googleUrl);
         }
     }
 }
